cc10626036f18a1ef75f4060e451766e025f4327
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-04-28 18:09:34 -0500

    fixed cors, altered meme controller

--- app/controllers/api/memes_controller.rb
+++ app/controllers/api/memes_controller.rb
@@ -1,6 +1,6 @@
 class Api::MemesController < ApplicationController
 
-  before_action :authenticate_user, except: [:index, :show]
+  # before_action :authenticate_user, except: [:index, :show]
 
   def index
     @memes = Meme.all
@@ -13,36 +13,47 @@ class Api::MemesController < ApplicationController
   end
 
   def create
-    @meme = Meme.new(
-      top_text: params[:top_text],
-      bottom_text: params[:bottom_text],
-      img: params[:img],
-      user_id: current_user.id
-    )
+    @meme = Meme.create(meme_params)
+    render json: @meme
+  end
 
-    if @meme.save
-      render 'show.json.jb'
-    else 
-      render json: {errors: @memes.errors.full_messages}, status: :unprocessable_entity
+  private
+
+    def meme_params
+      params.require(:meme).permit(:top_text, :img, :bottom_text)
     end
-  end
 
-  def update
-    @meme = Meme.find(params[:id])
-    @meme.top_text = params[:top_text] || @meme.top_text
-    @meme.bottom_text = params[:bottom_text] || @meme.bottom_text
-    @meme.img = params[:img] || @meme.img
+  # def create
+  #   @meme = Meme.new(
+  #     top_text: params[:top_text],
+  #     bottom_text: params[:bottom_text],
+  #     img: params[:img],
+  #     user_id: current_user.id
+  #   )
 
-    if @meme.save
-      render 'show.json.jb'
-    else 
-      render json: {errors: @meme.errors.full_messages}, status: :unprocessable_entity
-    end
-  end
+  #   if @meme.save
+  #     render 'show.json.jb'
+  #   else 
+  #     render json: {errors: @memes.errors.full_messages}, status: :unprocessable_entity
+  #   end
+  # end
 
-  def delete
-    @meme = Meme.find(params[:id])
-    @meme.destroy
-    render json: {message: "Meme successfully destroyed."}
-  end
+#   def update
+#     @meme = Meme.find(params[:id])
+#     @meme.top_text = params[:top_text] || @meme.top_text
+#     @meme.bottom_text = params[:bottom_text] || @meme.bottom_text
+#     @meme.img = params[:img] || @meme.img
+
+#     if @meme.save
+#       render 'show.json.jb'
+#     else 
+#       render json: {errors: @meme.errors.full_messages}, status: :unprocessable_entity
+#     end
+#   end
+
+#   def delete
+#     @meme = Meme.find(params[:id])
+#     @meme.destroy
+#     render json: {message: "Meme successfully destroyed."}
+#   end
 end
