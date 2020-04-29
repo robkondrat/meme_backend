12267c65d57e78a0e3f9fdb0e2c16b10f15a5cca
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-04-29 13:28:14 -0500

    new meme button edits

--- app/controllers/api/memes_controller.rb
+++ app/controllers/api/memes_controller.rb
@@ -12,31 +12,32 @@ class Api::MemesController < ApplicationController
     render 'show.json.jb'
   end
 
-  def create
-    @meme = Meme.create(meme_params)
-    render json: @meme
-  end
-
-  private
-
-    def meme_params
-      params.require(:meme).permit(:top_text, :img, :bottom_text)
-    end
-
   # def create
-  #   @meme = Meme.new(
-  #     top_text: params[:top_text],
-  #     bottom_text: params[:bottom_text],
-  #     img: params[:img],
-  #     user_id: current_user.id
-  #   )
-
-  #   if @meme.save
-  #     render 'show.json.jb'
-  #   else 
-  #     render json: {errors: @memes.errors.full_messages}, status: :unprocessable_entity
-  #   end
+  #   @meme = Meme.create(meme_params)
+  #   render json: @meme
   # end
+
+  # private
+
+  #   def meme_params
+  #     params.require(:meme).permit(:top_text, :img, :bottom_text, :user_id)
+  #   end
+
+  def create
+    @meme = Meme.new(
+      top_text: params[:top_text],
+      bottom_text: params[:bottom_text],
+      img: params[:img],
+      user_id: 1
+      # user_id: current_user.id
+    )
+
+    if @meme.save
+      render 'show.json.jb'
+    else 
+      render json: {errors: @memes.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
 
 #   def update
 #     @meme = Meme.find(params[:id])
